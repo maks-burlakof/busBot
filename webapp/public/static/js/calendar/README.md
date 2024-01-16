63ff8b3372fd12d81a8d34bd62e6abc90155eb9a
maks-burlakof <maks.burlakof@gmail.com>
2024-01-16 03:11:17 +0300

    webapp: Index, search, details
    Full calendar support

--- webapp/public/static/js/calendar/README.md
+++
@@ -1,1 +0,0 @@
-https://www.cssscript.com/multilingual-calendar-component/
--- webapp/public/static/js/find.js
+++ webapp/public/static/js/find.js
@@ -1,4 +1,26 @@
-let chosenDate = undefined;
+let fetchedData = null;
+let fetchedRoute = null;
+
+class URLParam {
+   static get(param, url) {
+      if (!url) {
+         url = window.location.href;
+      }
+      return new URL(url).searchParams.get(param);
+   }
+
+   static set(param, value) {
+      let urlObj = new URL(window.location.href);
+      urlObj.searchParams.set(param, value);
+      window.history.replaceState({path: `${urlObj}`}, '', urlObj);
+   }
+
+   static remove(param) {
+      let urlObj = new URL(window.location.href);
+      urlObj.searchParams.delete(param);
+      window.history.replaceState({path: `${urlObj}`}, '', urlObj);
+   }
+}
 
 let buttonModes = {
    'search': 'Найти',
@@ -6,52 +28,144 @@ let buttonModes = {
    'track': 'Отслеживать',
 }
 
-function allowSearching() {
-   Telegram.WebApp.MainButton.setParams({
-      'text': buttonModes['search'],
-      'is_active': true,
-      'is_visible': true,
-   });
-}
-
-function disallowSearching() {
-   Telegram.WebApp.MainButton.setParams({
-      'text': buttonModes['search'],
-      'is_active': false,
-      'is_visible': true,
-   });
+function isAllowSearching() {
+   if (URLParam.get('date') && URLParam.get('from') && URLParam.get('to')) {
+      Telegram.WebApp.MainButton.setParams({
+         'text': buttonModes['search'],
+         'is_active': true,
+         'is_visible': true,
+      });
+      Telegram.WebApp.MainButton.onClick(renderSearchResults);
+      return true;
+   } else {
+      Telegram.WebApp.MainButton.setParams({
+         'text': buttonModes['search'],
+         'is_active': false,
+         'is_visible': true,
+      });
+      return false;
+   }
 }
 
 function renderSearchIndex() {
    $('.items').css('transform', `translateX(0%) translateX(-8px)`);
-   // if date and routes are chosen
-   disallowSearching();
+   Telegram.WebApp.BackButton.hide();
+   isAllowSearching();
 }
 
 async function makeSearchRequest() {
-   let response = await fetch(`/api/search?date=${chosenDate}&city_from=${$("cityFrom")}&city_to=${$("cityTo")}`);
-   if (response.status !== 200) {
+   fetchedRoute = {
+      'date': URLParam.get('date'),
+      'from': URLParam.get('from'),
+      'to': URLParam.get('to'),
+   }
+   Telegram.WebApp.MainButton.disable();
+   Telegram.WebApp.MainButton.showProgress();
+   let response = await fetch(`/api/search?date=${fetchedRoute['date']}&city_from=${fetchedRoute['from']}&city_to=${fetchedRoute['to']}`);
+   if (!response.ok) {
       Telegram.WebApp.HapticFeedback.notificationOccurred('error');
       Telegram.WebApp.showConfirm('Не удалось выполнить запрос... Перезагрузим страничку?', location.reload);
+      return;
    }
+   fetchedData = await response.json();
+   Telegram.WebApp.MainButton.hideProgress();
+   Telegram.WebApp.MainButton.enable();
 }
 
-function renderSearchResults() {
-   Telegram.WebApp.MainButton.showProgress();
-   //
-   $('.items').css('transform', `translateX(100%) translateX(-24px)`);
-   Telegram.WebApp.MainButton.hideProgress();
-   Telegram.WebApp.MainButton.hide();
+async function renderSearchResults() {
+   let searchedRoute = {
+      'date': URLParam.get('date'),
+      'from': URLParam.get('from'),
+      'to': URLParam.get('to'),
+   }
+   $('#searchRouteText').html(`${searchedRoute['from']} <i class="fa-solid fa-arrow-right-long"></i> ${searchedRoute['to']}<br>${searchedRoute['date']}`);
+   $('#searchRouteContainer').html('');
+   $('.items').css('transform', `translateX(-100%) translateX(-24px)`);
+   Telegram.WebApp.HapticFeedback.impactOccurred('light');
    Telegram.WebApp.BackButton.show();
    Telegram.WebApp.BackButton.onClick(renderSearchIndex);
+   if (!fetchedData || fetchedRoute !== searchedRoute) {
+      isAllowSearching();
+      await makeSearchRequest();
+   }
+   for (let i = 1; i <= Object.keys(fetchedData['results']).length; i++) {
+      let data = fetchedData['results'][i];
+      $('#searchRouteContainer').append(`
+         <div class="input bus" data-size="xl">
+            <div class="bus-col-1">
+               <h4>${data['departure_time']} <span><i class="fa-solid fa-minus"></i> ${data['arrival_time']}</span></h4>
+               <p>${data['free_seats_text']}${data['free_seats'] ? (', ' + data['price'] + ' р.') : ''}</p>
+            </div>
+            <div class="bus-col-2">
+               <h3>${data['free_seats'] ? ('<i class="fa-solid fa-circle-check" style="color: #4bd501;"></i>') : ('<i class="fa-solid fa-circle-xmark" style="color: #ff0000;"></i>')}</h3>
+               ${data['free_seats'] ? ('<a>Заказать <i class="fa-solid fa-angle-right"></i></a>') : ('<a>Отслеживать <i class="fa-solid fa-angle-right"></i></a>')}
+            </div>
+         </div>
+`     );
+   }
+   Telegram.WebApp.MainButton.hide();
+}
+
+async function renderBusDetails() {
+   let searchedRoute = {
+      'date': URLParam.get('date'),
+      'from': URLParam.get('from'),
+      'to': URLParam.get('to'),
+   }
+   let searchedTime = URLParam.get('time');
+   $('#busDetailsText').html(`${searchedRoute['from']} <i class="fa-solid fa-arrow-right-long"></i> ${searchedRoute['to']}<br>${searchedRoute['date']} <span>(Нд)</span><br>${searchedTime}`);
+   $('#busDetailsContainer').html('');
+   $('.items').css('transform', `translateX(-200%) translateX(-40px)`);
+   Telegram.WebApp.HapticFeedback.impactOccurred('light');
+   Telegram.WebApp.BackButton.show();
+   Telegram.WebApp.BackButton.onClick(renderSearchResults);
+   if (!fetchedData || fetchedRoute !== searchedRoute) {
+      isAllowSearching();
+      await makeSearchRequest();
+   }
+   for (let i = 1; i <= Object.keys(fetchedData['results']).length; i++) {
+      let data = fetchedData['results'][i];
+      if (data['departure_time'] === searchedTime) {
+         $('#busDetailsText').append(`<span> - ${data['arrival_time']}</span>`);
+         $('#busDetailsContainer').html(`
+            <p>${data['free_seats_text']}<br>${data['price']} р.</p>
+            <p>${data['driver']['name']}<br>${data['driver']['phone']}</p>
+            <p>${data['name']}</p>
+            <div class="content content-rounded" style="background: var(--tok-stinger); margin-top: 0;">
+               <h3>Остановки</h3>
+               ${$('<p>').text(data['departure_stops'].map(item => `${item.name}: ${item.time}`).join(', '))}
+            </div>
+         `);
+         if (data['free_seats']) {
+            Telegram.WebApp.MainButton.setParams({
+               'text': buttonModes['buy'],
+               'is_active': true,
+               'is_visible': true,
+            });
+         } else {
+            Telegram.WebApp.MainButton.setParams({
+               'text': buttonModes['track'],
+               'is_active': true,
+               'is_visible': true,
+            });
+         }
+         return;
+      }
+   }
+   $('#busDetailsContainer').html(`Информации об этом рейсе не найдено`);
+   Telegram.WebApp.HapticFeedback.notificationOccurred('error');
+   Telegram.WebApp.showConfirm('Информация по этому рейсу отсутствует. Перейти в поиск?', () => {
+      window.location.pathname = '/find';
+      window.location.search = '';
+   });
 }
 
 $(document).ready(function() {
-   Telegram.WebApp.ready();
-   Telegram.WebApp.expand();
-   renderSearchIndex();
+   $('html').attr('data-theme', 'dark')
+   if (Telegram.WebApp.initData) {
+      $('html').data('theme', 'dark');
+   }
 
-   //calendar
    let today = new Date;
    const calendar = new VanillaCalendar('#calendar', {
       type: 'default',
@@ -62,19 +176,61 @@ $(document).ready(function() {
       },
       actions: {
          clickDay(event, dates) {
-            // haptic
-            chosenDate = dates['selectedDates'][0];
-            console.log(chosenDate);
+            Telegram.WebApp.HapticFeedback.selectionChanged();
+            let chosenDate = dates['selectedDates'][0];
             if (chosenDate) {
-               allowSearching();
+               URLParam.set('date', chosenDate);
+               isAllowSearching();
             } else {
-               disallowSearching();
+               URLParam.remove('date');
+               isAllowSearching();
             }
          },
       },
       settings: {
          lang: 'ru',
       },
+      CSSClasses: {
+         calendar: 'my-vanilla-calendar',
+         dayBtn: 'my-vanilla-calendar-day__btn',
+         dayBtnNext: 'my-vanilla-calendar-day__btn_next',
+      }
    });
+
+   if (URLParam.get('date')) {
+      calendar.settings.selected.dates = [URLParam.get('date')];
+   }
    calendar.init();
+   if (URLParam.get('from')) {
+      $('#cityFrom').val(URLParam.get('from'));
+   } else {
+      URLParam.set('from', $('#cityFrom').val());
+   }
+   if (URLParam.get('to')) {
+      $('#cityTo').val(URLParam.get('to'));
+   } else {
+      URLParam.set('to', $('#cityTo').val());
+   }
+
+   if (URLParam.get('date') && URLParam.get('from') && URLParam.get('to') && URLParam.get('time')) {
+      renderBusDetails();
+   } else if (URLParam.get('date') && URLParam.get('from') && URLParam.get('to')) {
+      renderSearchResults();
+   } else {
+      renderSearchIndex();
+   }
+
+   Telegram.WebApp.ready();
+   Telegram.WebApp.expand();
+
+   $('#cityFrom').change(function () {
+      Telegram.WebApp.HapticFeedback.selectionChanged();
+      URLParam.set('from', $('#cityFrom').val());
+      isAllowSearching();
+   });
+   $('#cityTo').change(function () {
+      Telegram.WebApp.HapticFeedback.selectionChanged();
+      URLParam.set('to', $('#cityTo').val());
+      isAllowSearching();
+   });
 });
